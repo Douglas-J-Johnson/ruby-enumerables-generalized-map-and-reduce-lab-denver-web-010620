a8530ea0ade33993a3da60d851a6806c5cfacb62
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-29 02:13:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_point=nil)
   #puts source_array
   #puts "starting_point: " + starting_point
   start_index = 0
-  
+
   if starting_point then
     return_value = starting_point
   else

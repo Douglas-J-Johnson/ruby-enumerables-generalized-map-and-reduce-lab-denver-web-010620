a7ea511040dd90dd52aa5b2f789fd4fd73e127a8
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-29 02:13:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,11 +8,17 @@ def map(source_array)
   return return_array
 end
 
-def reduce(source_array, starting_point=0)
+def reduce(source_array, starting_point=nil)
   #puts source_array
   #puts "starting_point: " + starting_point
-
-  return_value = starting_point
+  start_index = 0
+  
+  if starting_point then
+    return_value = starting_point
+  else
+    return_value = source_array[start_index]
+    start_index = start_index + 1
+  end
 
   for i in 0...source_array.length
     return_value = yield(return_value, source_array[i])

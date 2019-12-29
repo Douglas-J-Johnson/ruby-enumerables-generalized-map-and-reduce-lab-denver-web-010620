653c8bae5eb2d201046fda72207f877905f3d591
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-29 02:11:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+def map(source_array)
+  return_array = []
+
+  for i in 0...source_array.length
+    return_array[i] = yield(source_array[i])
+  end
+
+  return return_array
+end
+
+def reduce(source_array, starting_point=0)
+  #puts source_array
+  #puts "starting_point: " + starting_point
+
+  return_value = starting_point
+
+  for i in 0...source_array.length
+    return_value = yield(return_value, source_array[i])
+  end
+
+  #puts "return_value: " + return_value
+  return return_value
+end
